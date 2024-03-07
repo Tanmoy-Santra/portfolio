b8d2bb977e45d01f4249f39310d65aa53047829f
Tanmoy-Santra <tanmoysantra911@gmail.com>
2024-03-07 12:54:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+
+import Project from './components/Project';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import SendMsg from './components/SendMsg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>      
+    <Routes> 
+     <Route path='/about' element={<About/>} />
+     <Route path='/home' element={<Home/>} />
+     <Route path='/project' element={<Project/>} />          
+     <Route path='/resume' element={<Resume/>} />          
+     <Route path='/contact' element={<Contact/>} />          
+     <Route path='/' element={<Home/>}/> 
+      
+     <Route path='/sendmsg' element={<SendMsg/>}/> 
+
+    </Routes>
+    </Router>
+
+    {/* <About></About> */}
     </div>
   );
 }
